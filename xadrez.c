153f5f900b3aab2855b2040f9e97566e92903d95
LuizGeffer <99416410+LuizGeffer@users.noreply.github.com>
2026-02-09 13:21:20 +0000

    Finalização do Desafio Mestre

--- xadrez.c
+++ xadrez.c
@@ -5,24 +5,28 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 
-void torre( int casas) {
+void torre( int casas) { //Movimentação da Torre
     for(int x=0; x<casas; x++){
         printf("Direita\n");
        }
 }
 
-void bispo(int casas) {
-    for (int i = 0; i < casas; i++) {
+void bispo(int casas) {//Movimentação do Bispo
+    for (int i = 0; i < casas; i++) { // loop externo para a direita
         printf("Direita\n");
+         for (int j = casas - 1; j < casas; j++) {  // loop externo para cima     
         printf("Cima\n");
+    }
     }
 }
 
-void rainha( int casas) {
+void rainha( int casas) {//Movimentação da Rainha
     for(int x=0; x<casas; x++){
         printf("Esquerda\n");
        }
 }
+
+
 
 
 int main() {
@@ -32,7 +36,7 @@ int main() {
    bispo(5); // Chamando a função bispo para movimentar 5 casas 
    printf("\n");//Linha de sepação de peças
    rainha(8); // Chamando a função rainha para movimentar 8 casas a esquerda
-    
+   printf("\n");//Linha de sepação de peças
 
 
     
@@ -42,13 +46,13 @@ int main() {
   
    
 
-    printf("\nCavalo\n\n");//Declarando a peça Cavalo
-    for (int y = 0; y < 2; y++) { //Cavalo 2 casas para Baixo
-        printf("Baixo\n");
+    printf("Cavalo\n\n");//Declarando a peça Cavalo
+    for (int y = 0; y < 2; y++) { //Cavalo 2 casas para Cima
+        printf("Cima\n");
 
         for (int x = 0; x<1 && y == 1 ; x++)
         {
-            printf("Esquerda\n"); //Cavalo 1 casa para Esquerda
+            printf("Direita\n"); //Cavalo 1 casa para Direita
         }
         
 
